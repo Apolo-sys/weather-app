c6dc94bc8886fdeefacb6440035a1497c2d44bff
apolo-sys <cristian124421@gmail.com>
2021-04-11 23:40:05 +0200

    Updated index

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+const Searchs = require("./models/searchs");
 const {readInput, inquirerMenu, pause} = require("./helpers/inquirer");
 
 
 const main = async () => {
 
+    const searchs = new Searchs()
     let menuOpt;
 
     do {
 
         menuOpt = await inquirerMenu()
-        console.log({ menuOpt })
+
+        switch ( menuOpt ) {
+
+            case 1:
+                // Show Message
+                const site = await readInput('Ciudad:');
+                console.log(site);
+
+                // Search Sites
+
+                // Selects One Site
+
+                // Get Weather
+
+                // Show Info
+                console.log("\nInformación de la ciudad\n".green);
+                console.log("Ciudad:", );
+                console.log('Lat:', );
+                console.log('Lng:', );
+                console.log('Temperatura:', );
+                console.log('Minima:', );
+                console.log('Maxima:', );
+
+            break;
+
+        }
 
 
         if (menuOpt !== 0) await pause()
---
+++ models/searchs.js
@@ -0,0 +1,26 @@
+
+
+
+class Searchs {
+
+    history = ['Tegucigalpa', 'Madrid', 'San jose'];
+
+    constructor() {
+        // TODO: Read DB If Exists
+    }
+
+    async city(site = '') {
+
+        // Peticion http
+        console.log(site)
+
+
+        return []; // Devolver los lugares que coincidan con "site"
+    }
+
+}
+
+
+
+
+module.exports = Searchs
