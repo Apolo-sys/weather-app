c7b80a0e2d815aee0758e7d596b97ad5856756f5
apolo-sys <cristian124421@gmail.com>
2021-04-12 03:03:37 +0200

    Created instance of AXIOS MapBox

--- models/searchs.js
+++ models/searchs.js
@@ -9,12 +9,26 @@ class Searchs {
         // TODO: Read DB If Exists
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiY3Jpc3RpYW5yZzM2IiwiYSI6ImNrbmR1Z2Q4ZzAweW0yb253MWNlcDl1Y3QifQ.oTLxrkDNIgPBGKpBxEshqg',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async citySearch(site = '') {
 
         try {
             // Peticion http
-            // console.log('Ciudad:', site)
-            const {data} = await axios.get('https://reqres.in/api/users?page=2')
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ site }.json`,
+                params: this.paramsMapbox
+            })
+
+            const { data } = await instance.get();
+
+
             console.log(data)
 
             return []; // Devolver los lugares que coincidan con "site"
